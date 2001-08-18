0af6c0c6fdeb121f8dd47744772f2399a12204c3
Sam Ruby <rubys@apache.org>
2001-08-18 02:12:17 +0000

    Arg!  Now get rid of extra CR's!

--- gen.java
+++ gen.java
@@ -41,14 +41,14 @@ public class gen {
       * @return Node
       */
     private Document parse(String source) throws Exception {
-        try {
-            DocumentBuilder dBuilder = dFactory.newDocumentBuilder();
-            return dBuilder.parse(new java.io.File(source));
-        } catch (SAXParseException e) {
-            System.err.print("Error parsing file " + source);
-            System.err.println(" line " + e.getLineNumber() + ": ");
-            throw e;
-        }
+	try {
+	    DocumentBuilder dBuilder = dFactory.newDocumentBuilder();
+	    return dBuilder.parse(new java.io.File(source));
+	} catch (SAXParseException e) {
+	    System.err.print("Error parsing file " + source);
+	    System.err.println(" line " + e.getLineNumber() + ": ");
+	    throw e;
+	}
     }
 
     /**
@@ -58,11 +58,11 @@ public class gen {
       * @return Node
       */
     private Node transform(Node dom, String sheet) throws Exception {
-        StreamSource source = new StreamSource("stylesheet/"+sheet);
-        Transformer transformer = tFactory.newTransformer(source);
-        DOMResult output = new DOMResult();
-        transformer.transform(new DOMSource(dom), output);
-        return output.getNode();
+	StreamSource source = new StreamSource("stylesheet/"+sheet);
+	Transformer transformer = tFactory.newTransformer(source);
+	DOMResult output = new DOMResult();
+	transformer.transform(new DOMSource(dom), output);
+	return output.getNode();
     }
 
     /**
@@ -73,17 +73,17 @@ public class gen {
     private void copyChildren(Element source, Element target) {
        Node child=source.getFirstChild();
        while (child != null) {
-           Node next=child.getNextSibling();
-           target.appendChild(child);
-           child=next;
+	   Node next=child.getNextSibling();
+	   target.appendChild(child);
+	   child=next;
        }
 
        NamedNodeMap attrs = source.getAttributes();
        for (int i=0; i<attrs.getLength(); i++) {
-           Node a = attrs.item(i);
-           if (target.getAttributeNode(a.getNodeName())==null) {
-               target.setAttribute(a.getNodeName(),a.getNodeValue());
-           }
+	   Node a = attrs.item(i);
+	   if (target.getAttributeNode(a.getNodeName())==null) {
+	       target.setAttribute(a.getNodeName(),a.getNodeValue());
+	   }
        }
     }
 
@@ -95,36 +95,36 @@ public class gen {
        // expand hrefs
        Attr href = node.getAttributeNode("href");
        if (href != null && !node.getNodeName().equals("url")) {
-           String source=href.getValue();
-           Node sub = parse(source);
+	   String source=href.getValue();
+	   Node sub = parse(source);
 
-           if (source.lastIndexOf(".")>0)
-               source=source.substring(0,source.lastIndexOf("."));
+	   if (source.lastIndexOf(".")>0)
+	       source=source.substring(0,source.lastIndexOf("."));
 
-           if (source.lastIndexOf("/")>0)
-               source=source.substring(source.lastIndexOf("/")+1);
+	   if (source.lastIndexOf("/")>0)
+	       source=source.substring(source.lastIndexOf("/")+1);
 
-           node.removeAttribute("href");
-           node.setAttribute("defined-in", source);
+	   node.removeAttribute("href");
+	   node.setAttribute("defined-in", source);
 
-           Document doc = node.getOwnerDocument();
-           Element copy=(Element)doc.importNode(sub.getFirstChild(), true);
-           copyChildren(node, copy);
+	   Document doc = node.getOwnerDocument();
+	   Element copy=(Element)doc.importNode(sub.getFirstChild(), true);
+	   copyChildren(node, copy);
 
-           Element parent = (Element)node.getParentNode();
-           if (node.getNodeName().equals("profile")) {
-               copyChildren(copy, parent);
-           } else {
-               parent.replaceChild(copy,node);
-           }
+	   Element parent = (Element)node.getParentNode();
+	   if (node.getNodeName().equals("profile")) {
+	       copyChildren(copy, parent);
+	   } else {
+	       parent.replaceChild(copy,node);
+	   }
        }
 
        // recurse through the children
        Node child=node.getFirstChild();
        while (child != null) {
-           Node next=child.getNextSibling();
-           if (child.getNodeType()==Node.ELEMENT_NODE) expand((Element)child);
-           child=next;
+	   Node next=child.getNextSibling();
+	   if (child.getNodeType()==Node.ELEMENT_NODE) expand((Element)child);
+	   child=next;
        }
     }
 
@@ -132,29 +132,29 @@ public class gen {
       * Merge the contents of nodes with the same value for the name attribute.
       * Attributes from later definitions get added (or overlay) prior
       * definitions.  Elements get appended.
-      * @param type Element localname.  Typically project or repository.
+      * @param type Element localname.	Typically project or repository.
       * @param list Hashtable used for recursion.  Must initially be empty.
       * @param Node Starting point for search.
       */
     private void merge(String type, Hashtable list, Node parent) {
        Node child=parent.getFirstChild();
        while (child != null) {
-           Node next=child.getNextSibling();
-           if (child.getNodeName().equals(type)) {
-               Element element = (Element) child;
-               String name = element.getAttributeNode("name").getValue();
+	   Node next=child.getNextSibling();
+	   if (child.getNodeName().equals(type)) {
+	       Element element = (Element) child;
+	       String name = element.getAttributeNode("name").getValue();
 
-               Element priorDefinition = (Element)list.get(name);
-               if (priorDefinition == null) {
-                   list.put(name, element);
-                   merge(type, list, element);
-               } else if (priorDefinition != element) {
-                   element.getParentNode().removeChild(element);
-                   copyChildren(element, priorDefinition);
-                   element=priorDefinition;
-               }
-           }
-           child=next;
+	       Element priorDefinition = (Element)list.get(name);
+	       if (priorDefinition == null) {
+		   list.put(name, element);
+		   merge(type, list, element);
+	       } else if (priorDefinition != element) {
+		   element.getParentNode().removeChild(element);
+		   copyChildren(element, priorDefinition);
+		   element=priorDefinition;
+	       }
+	   }
+	   child=next;
        }
     }
 
@@ -164,22 +164,22 @@ public class gen {
       * all matching nodes which contain the same value for the name attribute.
       * For elements that get "hoisted", an additional "defined-in" attribute
       * is added indicating where the element was originally defined.
-      * @param type Element localname.  Typically project or repository.
+      * @param type Element localname.	Typically project or repository.
       * @param Node Root (workspace) node
       */
     private void flatten(String type, Node root) {
-        Hashtable list = new Hashtable();
-        merge(type, list, root);
-        for (Enumeration e=list.keys(); e.hasMoreElements();) {
-           Element element = (Element)list.get(e.nextElement());
-           Element parent  = (Element)element.getParentNode();
-           if (parent != root) {
-               parent.removeChild(element);
-               String definedIn = parent.getAttributeNode("name").getValue();
-               element.setAttribute("defined-in",definedIn);
-               root.appendChild(element);
-           }
-        }
+	Hashtable list = new Hashtable();
+	merge(type, list, root);
+	for (Enumeration e=list.keys(); e.hasMoreElements();) {
+	   Element element = (Element)list.get(e.nextElement());
+	   Element parent  = (Element)element.getParentNode();
+	   if (parent != root) {
+	       parent.removeChild(element);
+	       String definedIn = parent.getAttributeNode("name").getValue();
+	       element.setAttribute("defined-in",definedIn);
+	       root.appendChild(element);
+	   }
+	}
     }
 
     /**
@@ -188,40 +188,40 @@ public class gen {
       * @param destination file
       */
     private void output(Node dom, String dest) throws Exception {
-        DOMSource in = new DOMSource(dom);
-        StreamResult out = new StreamResult(dest);
-        tFactory.newTransformer().transform(in, out);
+	DOMSource in = new DOMSource(dom);
+	StreamResult out = new StreamResult(dest);
+	tFactory.newTransformer().transform(in, out);
     }
 
     /**
-      * Replace ant "depend" elements with "property" elements.  This is
+      * Replace ant "depend" elements with "property" elements.	 This is
       * a convenience "syntatic sugar" that makes for simpler project
       * definitions.  Attribute "property" becomes name.  Attributes
       * reference="jarpath" and classpath="true" are added.
       * @param document to be transformed
       */
     private void antDependsToProperties(Document document) throws Exception {
-        NodeIterator nl = XPathAPI.selectNodeIterator(document, "//ant/depend");
-        for (Node depend=nl.nextNode(); depend!=null; depend=nl.nextNode()) {
+	NodeIterator nl = XPathAPI.selectNodeIterator(document, "//ant/depend");
+	for (Node depend=nl.nextNode(); depend!=null; depend=nl.nextNode()) {
 
-            // create a new element based on existing element
-            Element property = document.createElement("property");
-            property.setAttribute("reference", "jarpath");
-            property.setAttribute("classpath", "add");
-            copyChildren((Element)depend, property);
+	    // create a new element based on existing element
+	    Element property = document.createElement("property");
+	    property.setAttribute("reference", "jarpath");
+	    property.setAttribute("classpath", "add");
+	    copyChildren((Element)depend, property);
 
-            // change property attribute to name attribute
-            if (property.getAttributeNode("name")==null) {
-               Attr pname = property.getAttributeNode("property");
-               if (pname != null) {
-                   property.setAttribute("name",pname.getValue());
-                   property.removeAttributeNode(pname);
-               }
-            }
+	    // change property attribute to name attribute
+	    if (property.getAttributeNode("name")==null) {
+	       Attr pname = property.getAttributeNode("property");
+	       if (pname != null) {
+		   property.setAttribute("name",pname.getValue());
+		   property.removeAttributeNode(pname);
+	       }
+	    }
 
-            // replace existing element with new one
-            depend.getParentNode().replaceChild(property, depend);
-        }
+	    // replace existing element with new one
+	    depend.getParentNode().replaceChild(property, depend);
+	}
     }
 
     /**
@@ -233,20 +233,20 @@ public class gen {
       */
     private void renameModuleToProject(Document document) throws Exception {
 
-        // safely get a list of all modules
-        NodeIterator nl = XPathAPI.selectNodeIterator(document, "//module");
-        Vector list = new Vector();
-        for (Node module=nl.nextNode(); module!=null; module=nl.nextNode()) {
-           list.add(module);
-        }
+	// safely get a list of all modules
+	NodeIterator nl = XPathAPI.selectNodeIterator(document, "//module");
+	Vector list = new Vector();
+	for (Node module=nl.nextNode(); module!=null; module=nl.nextNode()) {
+	   list.add(module);
+	}
 
-        // replace all elements in that list with projects
-        for (Enumeration e=list.elements(); e.hasMoreElements();) {
-           Element module = (Element)e.nextElement();
-           Element project = document.createElement("project");
-           copyChildren(module, project);
-           module.getParentNode().replaceChild(project, module);
-        }
+	// replace all elements in that list with projects
+	for (Enumeration e=list.elements(); e.hasMoreElements();) {
+	   Element module = (Element)e.nextElement();
+	   Element project = document.createElement("project");
+	   copyChildren(module, project);
+	   module.getParentNode().replaceChild(project, module);
+	}
     }
 
     /**
@@ -256,29 +256,29 @@ public class gen {
       * @return Node
       */
     private gen(String source) throws Exception {
-        Document workspace = parse(source);
-        expand((Element)workspace.getFirstChild());
-        renameModuleToProject(workspace);
-        flatten("project", workspace.getFirstChild());
-        flatten("repository", workspace.getFirstChild());
-        antDependsToProperties(workspace);
+	Document workspace = parse(source);
+	expand((Element)workspace.getFirstChild());
+	renameModuleToProject(workspace);
+	flatten("project", workspace.getFirstChild());
+	flatten("repository", workspace.getFirstChild());
+	antDependsToProperties(workspace);
 
-        Node resolved = transform(workspace, "defaults.xsl");
-        output (resolved, "work/merge.xml");
+	Node resolved = transform(workspace, "defaults.xsl");
+	output (resolved, "work/merge.xml");
 
-        Node sorted   = transform(resolved, "sortdep.xsl");
-        output (sorted, "work/sorted.xml");
+	Node sorted   = transform(resolved, "sortdep.xsl");
+	output (sorted, "work/sorted.xml");
     }
 
     public static void main(String[] args) {
-        try {
-            if (args.length == 1)
-                new gen(args[0]);
-            else
-                System.out.println("Usage: gen workspace.xml");
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(99);
-        }
+	try {
+	    if (args.length == 1)
+		new gen(args[0]);
+	    else
+		System.out.println("Usage: gen workspace.xml");
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    System.exit(99);
+	}
     }
 }
