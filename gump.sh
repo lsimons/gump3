a3ff1fe3f9ad2bc9de2a7c398aa926a968abab5c
Nick J. Chalko <nickchalko@apache.org>
2003-04-08 19:18:58 +0000

    Here is adams rework.  I moved the cvs log to the bottom, and added a comment about using a local_env.sh file.
    Commit notes from Adam Jack:
    
    I've taken your enhancements and tried to go one step further.
    
    I have this accepting three environment variables GUMP/GUMP_WS/GUMP_LOG and
    try to do everything off those. It exits (with 1 -- that right?) if they are
    not set. I set those three and it seems to run in my environment. Normally
    I'd expect folks to hack these entries.
    
    1) I have it copy `hostname -s`.xml (likely workspace) and 'likely profile'
    and the gump.sh to myprofile:
    
    	cp $GUMP/gump.sh $GUMP_PROFILE_LOG_DIR
    	cp $GUMP_HOST.xml  $GUMP_PROFILE_LOG_DIR
    	cp `grep profile $GUMP_HOST.xml  | cut -d\" -f2` $GUMP_PROFILE_LOG_DIR
    
    2) I added some CVS headers, but am not sure I got those right.
    
    3) I set it as bash not sh -- that ok?
    
    4) I made it echo some separators, but it could use more verbose information
    about each section.
    
    5) I made the output be gump.html (better name than gen.html).
    
    6) I don't like the way cron is detected (for running nag.pl) something
    better is needed.
    
    7) I made it do the CVS update each time, and a build clean each time. Maybe
    these are overkill.
    
    This is a major work in progress (it'd be nice if it output HTML as pretty
    as build.sh or update.sh) and it needs a few switches (so folks don't have
    to do everything every time.)
    
    This is is "Linux" format.
    
    
    Submitted by:	Adam Jack ajack@TrySybase.com

--- gump.sh
+++ gump.sh
@@ -1,45 +1,145 @@
-#! /bin/sh
+#!/bin/bash
+#
 # $Header: $
-export JAVA_HOME=/usr/java/j2sdk1.4.1_01
-export PATH=/home/nick/gump/bin:$JAVA_HOME/bin:/usr/local/bin:$PATH
-export CLASSPATH=$JAVA_HOME/lib/tools.jar
-export CVS_RSH=`which ssh`
-export LOG_DIR=/var/www/html/gump
-#export LOG=/home/nick/gump-log/gen.html
-export LOG=/var/www/html/gump/gen.html
-export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib
-export FORREST_HOME=/home/nick/xml-forrest/build/dist/shbat
+
+# nickchalko:  I thnik we can use  if  [-f local_env.sh ] . local_env.sh  to 
+# set all the stuff below
+# :USER: set GUMP and GUMP WORKSPACE and GUMP LOG DIR here
+#export GUMP=
+#export GUMP_WS=
+#export GUMP_LOG_DIR=
+# Examples...
+# export GUMP=$HOME/jakarta-gump
+# export GUMP_WS=/homelocal/build/gump-ws
+# export GUMP_LOG_DIR=$GUMP_WS/www
+
+#
+#
+# :USER: set your PATH and CLASSPATH and other ENV settings here
+# :USER: ... if you need to, for cron etc.
+#
+# Examples..
+#
+# export PATH=$GUMP/bin:$JAVA_HOME/bin:/usr/local/bin:$PATH
+# export CLASSPATH=$JAVA_HOME/lib/tools.jar
+# export CVS_RSH=`which ssh`
+# export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib
+# export FORREST_HOME=$HOME/xml-forrest/build/dist/shbat
+
+if [ ! $GUMP ] ; then
+	echo "Set the \$GUMP variable to your gump install"
+	exit 1
+fi
+
+if [ ! $GUMP_WS ] ; then
+	echo "Set the \$GUMP_WS variable to your gump install"
+	exit 1
+fi
+
+if [ ! $GUMP_LOG_DIR ] ; then
+	echo "Set the \$GUMP_LOG_DIR variable to your gump install"
+	exit 1
+fi
 
 
 
+#
+# Calculated
+#
+export GUMP_HOST=`hostname -s`
+export GUMP_DATE=`date`
+export GUMP_LOG=$GUMP_LOG_DIR/gump.html
+export GUMP_PROFILE_LOG_DIR=$GUMP_LOG_DIR/myprofile
+
+export SEPARATOR='------------------------------------------------------- G U M P'
+
+#
+# Generate gump.html from this (into the WWW site)
+#
 umask 002
+echo \<XMP\> > $GUMP_LOG
 
-echo \<XMP\> > $LOG
-cd /home/nick/local-gump
-#mkdir $LOG_DIR/myprofile
-cp csm.xml  $LOG_DIR/myprofile
-cp $HOME/cron/gump2.sh $LOG_DIR/myprofile
-cvs update  >> $LOG 2>&1 
-rm .timestamp
-bash gen.sh csm.xml >> $LOG 2>&1 
-#echo >> $LOG
-#bash work/build.sh gump clean >> $LOG
-echo >> $LOG
-export >> $LOG
+echo $SEPARATOR >> $GUMP_LOG
+echo $SEPARATOR >> $GUMP_LOG
+echo "Gump run on $GUMP_HOST at $GUMP_DATE" >> $GUMP_LOG
+echo $SEPARATOR >> $GUMP_LOG
+echo $SEPARATOR >> $GUMP_LOG
+echo >> $GUMP_LOG
+
+#
+# Store the profile (into a myprofile dir)
+#
+cd $GUMP
+if [ ! -d $GUMP_PROFILE_LOG_DIR ] ; then
+	mkdir $GUMP_PROFILE_LOG_DIR; 
+fi
+cp $GUMP/gump.sh $GUMP_PROFILE_LOG_DIR
+cp $GUMP_HOST.xml  $GUMP_PROFILE_LOG_DIR
+cp `grep profile $GUMP_HOST.xml  | cut -d\" -f2` $GUMP_PROFILE_LOG_DIR
+
+#
+# Do a CVS update
+#
+echo $SEPARATOR >> $GUMP_LOG
+cd $GUMP
+cd ..
+export CVSROOT=:pserver:anoncvs@cvs.apache.org:/home/cvspublic
+cvs update  >> $GUMP_LOG 2>&1 
+rm -f .timestamp
+
+
+#
+# Check the projects
+#
+cd $GUMP
+echo $SEPARATOR >> $GUMP_LOG
+ant check >> $GUMP_LOG 2>&1
+
+#
+# Do a gen
+#
+cd $GUMP
+echo $SEPARATOR >> $GUMP_LOG
+bash gen.sh tsbuild1.xml >> $GUMP_LOG 2>&1 
+echo >> $GUMP_LOG
+
+#
+# Do a clean
+#
+echo $SEPARATOR >> $GUMP_LOG
+bash $GUMP_WS/build.sh gump clean >> $GUMP_LOG
+echo >> $GUMP_LOG
+
+#
+# Capture environment
+#
+echo $SEPARATOR >> $GUMP_LOG
+export >> $GUMP_LOG
 
 # /usr/X11R6/bin/Xvfb :8 &
 # export DISPLAY=:8
 
-cd /home/nick/gump-work/
-bash update.sh all   >> $LOG 2>&1 
-rm /home/nick/gump-jars
-bash build.sh all   >> $LOG 2>&1 
+#
+# Do an update (from CVS)
+#
+cd $GUMP_WS
+echo $SEPARATOR >> $GUMP_LOG
+bash update.sh all   >> $GUMP_LOG 2>&1 
 
-cd $HOME/local-gump/
-echo started from cron $STARTED_FROM_CRON
+#
+# Do the build
+#
+cd $GUMP_WS
+echo $SEPARATOR >> $GUMP_LOG
+bash build.sh all   >> $GUMP_LOG 2>&1 
+
+cd $GUMP
+echo $SEPARATOR >> $GUMP_LOG
 if [ -n $STARTED_FROM_CRON ] ; then 
-	perl nag.pl work/naglist >> $LOG 2>&1
+	perl nag.pl work/naglist >> $GUMP_LOG 2>&1
 fi;
 
-echo \</XMP\> >> $LOG
+echo \</XMP\> >> $GUMP_LOG
 pkill -P $$ 
+
+# $Log: $
