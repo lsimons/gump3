4a1d1adf199d048c6a6848c5332baa22adf78708
Adam R. B. Jack <ajack@apache.org>
2003-09-05 05:25:54 +0000

    Type logdir when meant tmpdir

--- python/gump/model.py
+++ python/gump/model.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-# $Header: /home/stefano/cvs/gump/python/gump/Attic/model.py,v 1.14 2003/09/05 05:20:42 ajack Exp $
-# $Revision: 1.14 $
-# $Date: 2003/09/05 05:20:42 $
+# $Header: /home/stefano/cvs/gump/python/gump/Attic/model.py,v 1.15 2003/09/05 05:25:54 ajack Exp $
+# $Revision: 1.15 $
+# $Date: 2003/09/05 05:25:54 $
 #
 # ====================================================================
 #
@@ -123,7 +123,8 @@ class Workspace(GumpModelObject):
     if not self['banner-image']:
       self['banner-image']=default.bannerimage
     if not self['banner-link']: self['banner-link']="http://jakarta.apache.org"
-    if not self.tmpdir: self.logdir=os.path.join(self.basedir,"tmp")
+    
+    if not self.tmpdir: self.tmpdir=os.path.join(self.basedir,"tmp")
     if not os.path.exists(self.tmpdir): os.mkdir(self.tmpdir)
     
     if not self.logdir: self.logdir=os.path.join(self.basedir,"log")
