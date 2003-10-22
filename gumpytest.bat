1acf1181c9386791d2bd5ef1b7e454fae3777394
Adam R. B. Jack <ajack@apache.org>
2003-10-22 20:00:50 +0000

    Gumpy needs self tests, these will launch eventually them.

---
+++ gumpytest.bat
@@ -0,0 +1,9 @@
+@echo off
+
+REM 
+REM    Copyright (c) 2000-2003 The Apache Software Foundation.  All rights
+REM    reserved.
+
+ENV
+CWD
+
---
+++ gumpytest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# This script will self-test Gump (Python) Capabilities
+#
+#
+# $Header: $
+
+export
+pwd
