4b7c472f55334498d4f2f6d8dff3a6ac2ece84f4
Adam R. B. Jack <ajack@apache.org>
2003-11-26 20:08:43 +0000

    Leo's gump install is within the workspace itself, so the rsynch keeps "cleaning up" his local configuration. Add it to CVS.

---
+++ local-env-py-lsd.sh
@@ -0,0 +1,12 @@
+export ROOT=/data/gump
+export GUMP=$ROOT/jakarta-gump
+export GUMP_WS=$ROOT/
+export GUMP_LOG_DIR=$ROOT/log
+
+export FORREST_HOME=/home/ajack/opt/forrest
+export PATH=$PATH:$FORREST_HOME/bin
+
+export JAVA_HOME=/opt/sunjdk
+export CLASSPATH=$JAVA_HOME/lib/tools.jar
+export PATH=$PATH:$JAVA_HOME/bin
+
