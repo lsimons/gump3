e0e577fe97a6899c6b4149bab862901f739eb724
Stefan Bodewig <bodewig@apache.org>
2004-02-27 09:30:47 +0000

    Add license

--- nag.pl
+++ nag.pl
@@ -1,5 +1,19 @@
 #!/usr/bin/perl
 
+# Copyright 2001-2004 The Apache Software Foundation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 #
 # Nag people and mailing lists upon particular build events,
 # typically failures.
@@ -13,8 +27,8 @@
 
 ($sec,$min,$hour,$mday,$mon,$year) = localtime;
 $date=sprintf("%d-%2.2d-%2.2d", $year+1900, $mon+1, $mday);
-$url = "http://gump.covalent.net/log";
-$home = "/data/gump/log";
+$url = "/home/bodewig/dev/gump/log";
+$home = "/home/bodewig/dev/gump/log";
 
 $sentto = {};
 
