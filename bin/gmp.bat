6aeecfca746d4f868465c163c561a800c1bf9461
Nicola Ken Barozzi <nicolaken@apache.org>
2004-04-10 08:50:30 +0000

    Use GUMP_HOME

--- bin/gmp.bat
+++ bin/gmp.bat
@@ -17,7 +17,7 @@ REM
 REM _ A P A C H E  G U M P _ A P A C H E   G U M P _ A P A C H E   G U M P _ 
 REM
 REM
-REM $Header: /home/stefano/cvs/gump/bin/gmp.bat,v 1.2 2004/04/09 12:52:02 nicolaken Exp $
+REM $Header: /home/stefano/cvs/gump/bin/gmp.bat,v 1.3 2004/04/10 08:50:30 nicolaken Exp $
 REM
 REM
 if "%OS%"=="Windows_NT" @SETLOCAL
@@ -29,7 +29,7 @@ IF EXIST  %LOCAL_ENV% CALL %LOCAL_ENV%
 SET HOST_LOCAL_ENV="local-env-py-%GUMP_HOST%.sh"
 IF EXIST  %HOST_LOCAL_ENV% CALL %HOST_LOCAL_ENV%
 
-CD ..
+CD %GUMP_HOME%
 python python\gmp.py %1 %2 %3 %4 %5
 
 if "%OS%"=="Windows_NT" @ENDLOCAL
@@ -39,6 +39,9 @@ if "%OS%"=="Windows_NT" @ENDLOCAL
 REM _ J A K A R T A  G U M P _ J A K A R T A  G U M P _ J A K A R T A  G U M P _ 
 REM
 REM $Log: gmp.bat,v $
+REM Revision 1.3  2004/04/10 08:50:30  nicolaken
+REM Use GUMP_HOME
+REM
 REM Revision 1.2  2004/04/09 12:52:02  nicolaken
 REM Bye bye jakarta!
 REM
