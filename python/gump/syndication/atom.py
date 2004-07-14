d5035a7c030e42fec3fc803e29d75e94df79399d
Adam R. B. Jack <ajack@apache.org>
2004-07-14 20:50:23 +0000

    Cleaning up includes has risks...

--- python/gump/syndication/atom.py
+++ python/gump/syndication/atom.py
@@ -114,7 +114,7 @@ class AtomFeed:
         log.debug("Atom News Feed to : " + self.file);         
         stream = open(self.file,'w')
         
-        modified=time.strftime('%Y-%m-%dT%H:%M:%SZ', gmtime())
+        modified=time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime())
         
         self.serializeToStream(stream,modified)
             
