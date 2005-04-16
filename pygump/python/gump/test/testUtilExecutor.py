7749dc2fbe659ecce1b2ebbdffc27e75b2b52697
Leo Simons <leosimons@apache.org>
2005-04-16 18:28:32 +0000

    Disable process cleanup test.
    * pygump/python/gump/test/testUtilExecutor.py: this still is not always consistently run last, so disable the process cleanup at-the-end testing.

--- pygump/python/gump/test/testUtilExecutor.py
+++ pygump/python/gump/test/testUtilExecutor.py
@@ -51,22 +51,25 @@ class ZZZExecutorUtilTestCase(TestCase):
         result = Popen(["pwd"], stdout=PIPE).communicate()[0]
         self.assertNotEqual("", result)
 
+        # Still not always running last. Need to fix the testrunner, until
+        # then, disable this...
+        #
         # This test can only be run once, since after the call to
         # "clean_up_processes" all future invocations of subprocesses
         # will fail. Therefore this class and its methods are awkwardly
         # named (the ZZZ prefix) so we "ensure" they run last. It's
         # ugly, I know.
-        processes = []
-        for i in range(0,10):
-            processes.append(Popen(["cat"], stdin=PIPE, stdout=PIPE))
-            
-        clean_up_processes(5)
-        for p in processes:
-            try:
-                pid, sts = os.waitpid(p.pid, os.WNOHANG)
-                self.assert_(os.WIFSIGNALED(exitcode), "Process should've been signalled...")
-            except:
-                pass
+        #processes = []
+        #for i in range(0,10):
+        #    processes.append(Popen(["cat"], stdin=PIPE, stdout=PIPE))
+        #    
+        #clean_up_processes(5)
+        #for p in processes:
+        #    try:
+        #        pid, sts = os.waitpid(p.pid, os.WNOHANG)
+        #        self.assert_(os.WIFSIGNALED(exitcode), "Process should've been signalled...")
+        #    except:
+        #        pass
         
 # this is used by testrunner.py to determine what tests to run
 def test_suite():
