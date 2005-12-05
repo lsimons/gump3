c156a3705de2f9fe5fa31b1b2b71ea34aaf3daae
Leo Simons <leosimons@apache.org>
2005-12-05 22:48:31 +0000

    Make it easy to have the gump codebase as an installed package.

---
+++ pygump/python/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2005 Leo Simons. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#     http://www.jicarilla.nl/licensing/
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
+from distutils.core import setup
+
+setup(name='gump',
+      version='3.0-alpha-5',
+      description='Apache Gump Core',
+      author='The Apache Gump Team',
+      author_email='general@gump.apache.org',
+      url='http://gump.apache.org/',
+      packages=['gump',
+                'gump.model',
+                'gump.plugins',
+                'gump.plugins.java',
+                'gump/util']
+     )
+
